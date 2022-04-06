75165c039bbed5ef7e7e45462d8a0b8768d8bf4d
Sergey Zhezlov <s1508v@gmail.com>
2022-04-06 23:28:53 +0300

    initial commit

---
+++ test.py
@@ -0,0 +1,16 @@
+
+
+def avg(x):
+    return sum(x) / len(x)  * 3
+
+
+
+def last(x):
+    return x[-1]
+
+
+some_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(avg(some_list))
+print('Made some changes')
+print('Some more changes'
